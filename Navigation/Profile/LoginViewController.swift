76392c6cc2051003a494f868366bb747a48858f2
Aleksei Serdiuk <aleksey.serduyk@gmail.com>
2022-08-30 17:48:34 +0600

    1-6 tasks is complited

---
+++ Navigation/Profile/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Алексей Сердюк on 30.08.2022.
+//  Copyright © 2022 aserdiuk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.navigationController?.navigationBar.isHidden = true
+
+    }
+}
+
