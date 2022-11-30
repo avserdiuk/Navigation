2830533a284837a6e46b4c4e88400653ff87af0e
Aleksei Serdiuk <aleksey.serduyk@gmail.com>
2022-11-30 20:46:37 +0600

    added logout

--- Navigation/Application layer/SceneDelegate.swift
+++ Navigation/Application layer/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -81,6 +82,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+
+         try? Auth.auth().signOut()
     }
     
     func sceneDidBecomeActive(_ scene: UIScene) {
